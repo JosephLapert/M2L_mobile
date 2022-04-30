dff180c9c487121f9b5996a3faaf1a62b8f10e8a
Lapertjoseph <lapertjoseph@gmail.com>
2022-04-30 10:52:12 +0200

    clean code

--- lib/api_call.dart
+++ lib/api_call.dart
@@ -22,7 +22,6 @@ class Users {
     final response = await http.get(
       Uri.parse('http://10.0.2.2:8000/api/users/'),
     );
-    print(response.body);
     if (response.statusCode == 200) {
       return Users.fromJson(json.decode(response.body)["success"][0][0]);
     } else {
